3160eb088755e5a909a6a3255ddb1d44dfe8e3d7
chuc4 <chuc4@rpi.edu>
2021-03-30 10:26:23 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Header";
+import Button from "./Components/Button";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/header' component={Header}/> 
+        <Route path='/button' component={Button}/>
+      </Switch>
+    </Router>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,61 @@
 import React, { useState } from "react";
 import "./Header.css";
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
 
-const Header = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+    color: 'blue',
+
+  },
+}));
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
+
+const Header = (props) => {
   const [counter, setCounter] = useState(0);
   console.log(counter);
+
+  const classes = useStyles();
 
   return (
     <>
       <button onClick={() => setCounter(counter + 1)} className="headerStyle">
-        Add
+        Add {props.name} 
       </button>
       {counter}
+
+      <div className={classes.root}>
+      <List component="nav" aria-label="main mailbox folders">
+        <ListItem button>
+          <ListItemText primary="Inbox" />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="Drafts" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List component="nav" aria-label="secondary mailbox folders">
+        <ListItem button>
+          <ListItemText primary="Trash" />
+        </ListItem>
+        <ListItemLink href="#simple-list">
+          <ListItemText primary="Spam" />
+        </ListItemLink>
+      </List>
+    </div>
     </>
   );
 };
+
+
 
 export default Header;
