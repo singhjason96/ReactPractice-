cc5f57065cef50c828a57dc9de8c1cdeb5abac2f
Jason Singh <jasonsingh@Jasons-MacBook-Pro.local>
2021-03-30 07:10:24 -0700

    new compoenent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Header";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Section from "./Components/Section";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/header" component={Header} />
+          <Route exact path="/section" component={Section} />
+        </Switch>
+      </Router>
     </div>
   );
 }
