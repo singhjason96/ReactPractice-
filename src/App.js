ae13d575f77d471300ee1bfd3e024a589ec6d238
Joanna Lao <jlao2044@gmail.com>
2021-03-30 11:02:04 -0400

    radio

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Header";
-<<<<<<< HEAD
-import TextBox from "./Components/TextBox";
-=======
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Section from "./Components/Section";
->>>>>>> cc5f57065cef50c828a57dc9de8c1cdeb5abac2f
+import AlertBox from "./Components/RadioB";
+import RadioB from "./Components/RadioB";
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <TextBox />
-=======
       <Router>
         <Switch>
           <Route exact path="/header" component={Header} />
           <Route exact path="/section" component={Section} />
+          <Route exact path="/radiob" component={RadioB} />
+
         </Switch>
       </Router>
->>>>>>> cc5f57065cef50c828a57dc9de8c1cdeb5abac2f
     </div>
   );
 }
