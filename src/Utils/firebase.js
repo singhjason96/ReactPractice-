cc060999f3001e36be492b5fd71d9ce6494dddbd
chuc4 <chuc4@rpi.edu>
2021-04-05 14:30:52 -0400

    firebase added

---
+++ src/Utils/firebase.js
@@ -0,0 +1,16 @@
+import React from "react";
+import firebase from "firebase/app";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBhXl4WYCjOyls9l6ONtwNUSHiukjOtTWs",
+    authDomain: "catherine-test-92e00.firebaseapp.com",
+    projectId: "catherine-test-92e00",
+    storageBucket: "catherine-test-92e00.appspot.com",
+    messagingSenderId: "965100338457",
+    appId: "1:965100338457:web:c26bf18e8963eb7db9d988",
+    measurementId: "G-WEFDWPK805"
+  };
+
+  
